2d374f3e0349ee4db0db04849687f6674d3bc5dd
Domas <domo.emailas@gmail.com>
2023-06-23 08:42:40 +0300

    simple navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
+import Navbar from '@/components/Navbar';
 import '@/styles/globals.css'
 
 const Root = ({ Component, children } : { Component: any, children: any }) => {
     return (
+        <div className='bg-slate-100'>
+            <Navbar />
             <Component {...children} />
+        </div>
     )
 }
 
